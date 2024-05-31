77d8ed0b4cffee495e88d91a37b5ed81b3208bdb
Harsjm <160833152+Harsjm@users.noreply.github.com>
2024-05-31 12:51:57 -0500

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,8 +1,3 @@
-resource "aws_iam_role" "eks_lb_controller" {
-  name = "eks_lb_controller"
-  assume_role_policy = data.aws_iam_policy_document.eks_lb_controller_assume_role.json
-}
-
 data "aws_iam_policy_document" "eks_lb_controller_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -13,7 +8,14 @@ data "aws_iam_policy_document" "eks_lb_controller_assume_role" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "eks_lb_controller_attach" {
-  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
-  role       = aws_iam_role.eks_lb_controller.name
+resource "aws_iam_role" "eks_lb_controller" {
+  name               = "eks_lb_controller"
+  assume_role_policy = data.aws_iam_policy_document.eks_lb_controller_assume_role.json
 }
+
+resource "aws_iam_role_policy_attachment" "eks_lb_controller_attach" {
+  role       = aws_iam_role.eks_lb_controller.name
+  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/AWSLoadBalancerControllerIAMPolicy"
+}
+
+data "aws_caller_identity" "current" {}
